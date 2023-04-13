56059dae11e823f5d06795da6adc192c9a78473c
aliergul <m.alieergul@gmail.com>
2023-04-13 22:00:31 +0300

    commit2

---
+++ hrmsExample/src/main/java/kodlamaio/hrmsExample/entities/concretes/User.java
@@ -0,0 +1,30 @@
+package kodlamaio.hrmsExample.entities.concretes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+}
