93077320ca70903b31c4c5fa61a86ab960ac90c5
aliergul <m.alieergul@gmail.com>
2023-04-13 21:24:52 +0300

    Uploading project.

---
+++ hrmsExample/src/main/java/kodlamaio/hrmsExample/HrmsExampleApplication.java
@@ -0,0 +1,13 @@
+package kodlamaio.hrmsExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsExampleApplication.class, args);
+	}
+
+}
